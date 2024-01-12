b292ef0ad72be54e6d141d8119fb2492d3a657d7
kdukhushi <khushi.rani@kickdrumtech.com>
2024-01-13 00:43:07 +0530

    setting github in azure, removing inner class

--- src/main/java/com/kdu/assignment1/Assignment1Application.java
+++ src/main/java/com/kdu/assignment1/Assignment1Application.java
@@ -6,17 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Assignment1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment1Application.class, args);
 	}
 
-	@RestController
-	class HelloController {
-		@GetMapping("/")
-		public String index() {
-			return "Hello, Khushi Rani!!!";
-		}
+
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello, Khushi Rani!!!";
+	}
+	@GetMapping("/hello")
+	public String index1() {
+		return "Hello, Khushi Rani!!!";
 	}
 }
